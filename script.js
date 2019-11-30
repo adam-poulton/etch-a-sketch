a2b60b8e43904362f72235cc82c38665d1391287
adam-poulton <adam.poulton5@gmail.com>
2019-11-30 20:13:26 +1100

    Implement basic 'painting'

--- script.js
+++ script.js
@@ -21,10 +21,25 @@ const createDivGrid = (rows, cols) => {
   grid.style.setProperty('--grid-cols', cols);
   for (let i = 0; i < (rows * cols); i++) {
     let cell = document.createElement('div')
+    //cell.addEventListener('mouseenter', setCellColor);
+    cell.addEventListener('mousedown', setCellColor);
+    cell.addEventListener('mouseover', setCellColor);
+    cell.addEventListener('contextmenu', (e) => {e.preventDefault();return false})
     grid.appendChild(cell).classList.add('grid-item')
   }
 }
 
-createDivGrid(16, 16);
+const setCellColor = (e) => {
+  if (e.buttons == 1) {
+    e.target.style.backgroundColor="red";
+  } else if (e.buttons == 2) {
+    e.target.style.backgroundColor="";
+  }
+  e.preventDefault();
+  return false;
+}
+
+
+createDivGrid(64, 64);
 window.onload = setContainerArea;
 window.onresize = setContainerArea;
